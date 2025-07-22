f367e9383b9cb08ef7f41b65fb7ccd1808555735
Komandr33 <and.kom2017@gmail.com>
2025-07-22 21:11:01 +0300

    create locations page

---
+++ pages/locations/index.tsx
@@ -0,0 +1,42 @@
+import { dehydrate, QueryClient } from "@tanstack/query-core";
+import { useQuery } from "@tanstack/react-query";
+import { LocationType, ResponseType } from "assets/api/rick-and-morty-api";
+import { Card } from "components/Card/Card";
+import { getLayout } from "components/Layout/Layout";
+import { PageWrapper } from "components/PageWrapper/PageWrapper";
+
+const getLocations = async () => {
+  return fetch("https://rickandmortyapi.com/api/location", {
+    method: "GET",
+  }).then((res: Response) => res.json());
+};
+
+export const getStaticProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.fetchQuery(["locations"], getLocations);
+
+  return {
+    props: {
+      dehydrateState: dehydrate(queryClient),
+    },
+  };
+};
+
+const Locations = () => {
+  const { data: locations } = useQuery<ResponseType<LocationType>>(
+    ["locations"],
+    getLocations
+  );
+
+  if (!locations) return null;
+
+  const locationsList = locations.results.map((location) => (
+    <Card key={location.id} name={location.name} />
+  ));
+
+  return <PageWrapper>{locationsList}</PageWrapper>;
+};
+
+Locations.getLayout = getLayout;
+export default Locations;
