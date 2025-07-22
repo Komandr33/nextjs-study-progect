a15cb10a1df9711d752a5f952d67cb050defa0aa
Komandr33 <and.kom2017@gmail.com>
2025-07-22 21:12:26 +0300

    fixed index.tsx from pages. Add Header and Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/image';
-import {NextPageWithLayout} from './_app';
-import {PageWrapper} from '../components/PageWrapper/PageWrapper';
-import {Header} from '../components/Header/Header';
+import { getLayout } from "components/Layout/Layout";
+import { PageWrapper } from "components/PageWrapper/PageWrapper";
+import Image from "next/image";
+import { NextPageWithLayout } from "./_app";
 
 const Home: NextPageWithLayout = () => (
-    <PageWrapper>
-        <Header/>
-        <Image
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-        />
-    </PageWrapper>
+  <PageWrapper>
+    <Image
+      src="/next.svg"
+      alt="Next.js Logo"
+      width={180}
+      height={37}
+      priority
+    />
+  </PageWrapper>
 );
 
+Home.getLayout = getLayout;
 export default Home;
